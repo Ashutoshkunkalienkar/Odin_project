e5402564bae41206b145c75a3dba9f665dbc159a
Ashutoshkunkalienkar <144326986+Ashutoshkunkalienkar@users.noreply.github.com>
2023-09-23 15:20:22 +0530

    adding bulb on off project

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,34 @@
 
+/**
+ * The function "hide" hides an element with the id "para" when a button with the id "btn" is clicked.
+ */
 function hide(){
     let btn = document.getElementById('btn');
     let para = document.getElementById('para');
         para.style.display = 'none';
 }
 
+/**
+ * The function "show" is used to display a hidden paragraph element when a button is clicked.
+ */
 function show(){
     let btn = document.getElementById('btn');
     let para = document.getElementById('para');
         para.style.display = 'block'
 }
 
+/**
+ * The function "hide" hides an element with the id "para1" when a button with the id "btn" is clicked.
+ */
 function hide(){
     let btn = document.getElementById('btn');
     let para = document.getElementById('para1');
         para.style.display = 'none';
 }
 
+/**
+ * The function "show" is used to display a hidden paragraph when a button is clicked.
+ */
 function show(){
     let btn = document.getElementById('btn');
     let para = document.getElementById('para1');
