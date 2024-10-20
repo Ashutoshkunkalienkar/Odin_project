5c02e4e7c30f4216ff9993b9fdea89704fecc203
Ashutoshkunkalienkar <144326986+Ashutoshkunkalienkar@users.noreply.github.com>
2024-10-20 12:19:59 +0530

    My first GSAP project

---
+++ project gsap/script.js
@@ -0,0 +1,126 @@
+var cursor = document.querySelector("#cursor")
+var parent = document.querySelector("#parent")
+
+parent.addEventListener("mousemove",(e)=>{
+    gsap.to(cursor,{
+        x:e.x,
+        y:e.y,
+        opacity:1,
+        duration:0.5,
+        ease:"back.out(1.7)"
+    })
+} )
+
+
+
+
+
+
+function animationMain1(){
+    var tl = gsap.timeline()
+
+tl.from("#logo img",{
+    opacity:0,
+    duration:0.5,
+    delay:0.4,
+    y:-50
+})
+
+tl.from("#logo h1",{
+    opacity:0,
+    duration:0.5,
+    y:-50
+})
+
+tl.from("#navlist span",{
+    opacity:0,
+    duration:0.5,
+    y:-50,
+    stagger:0.2
+})
+
+tl.from("#navlist button",{
+    opacity:0,
+    duration:0.5,
+    y:-100
+})
+
+
+tl.from(".text-1",{
+    opacity:0,
+    duration:0.5,
+    x:-100
+})
+
+
+tl.from(".text-2 p",{
+    opacity:0,
+    duration:0.3,
+    x:-100
+}, 'ab')
+
+tl.from("#pagePart2 img",{
+    opacity:0,
+    duration:0.3,
+    x:100
+},'ab')
+
+tl.from(".text-2 button",{
+    opacity:0,
+    duration:0.3,
+    x:-100
+})
+
+tl.from("#brands img",{
+    opacity:0,
+    duration:0.3,
+    y:-50,
+    stagger:0.2
+})
+
+}
+
+function animationMain2(){
+    var tl2 = gsap.timeline({
+        scrollTrigger:{
+            trigger:"#main2",
+            scroll:"body",
+            // markers:true,
+            start:"top 50%",
+            end:"top 0%",
+            scrub:2
+        }
+    })
+    
+    tl2.from(".service",{
+        x:-100,
+        duration:0.5,
+        opacity:0,
+    })
+    
+    
+    tl2.from(".card1.line1.left",{
+        x:-100,
+        duration:0.5,
+        opacity:0,
+    },'ab')
+    tl2.from(".card1.line2.right",{
+        x:100,
+        duration:0.5,
+        opacity:0,
+    },'ab')
+    tl2.from(".card1.line3.left",{
+        x:-100,
+        duration:0.5,
+        opacity:0,
+    },'a')
+    tl2.from(".card1.line4.right",{
+        x:100,
+        duration:0.5,
+        opacity:0,
+    },'a')
+}
+
+
+animationMain1()
+animationMain2()
